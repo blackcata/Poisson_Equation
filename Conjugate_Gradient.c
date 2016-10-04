f9288c59f5433dfdaa2b53a62fa42136a98f5445
Eugene <blackcata109@gmail.com>
2016-10-04 14:27:43 +0900

    Complete algorithm but boundary conditions have to be applied

--- Conjugate_Gradient.c
+++ Conjugate_Gradient.c
@@ -21,14 +21,59 @@ double func(int i, int j, double dx, double dy);
 void Conjugate_Gradient(double(*p)[COL],double dx, double dy, double tol)
 {
     int i,j,k,it;
+    double alpha,beta ;
+    
     double A[ROW*COL][ROW*COL] = {0};
+    double tmp[ROW*COL] = {0};
     
     double x[ROW*COL] = {0};
     double b[ROW*COL] = {0};
-    double r[ROW*COL] = {0};
     double z[ROW*COL] = {0};
-    double tmp[ROW*COL] = {0};
+    double r[ROW*COL] = {0};
+    double r_new[ROW*COL] = {0};
 
+    
+    make_Abx(A,b,x,p,dx,dy);
+    vmdot(A,x,tmp);
+    
+    for (i=0;i<ROW;i++){
+        for (j=0;j<COL;j++){
+            r[COL*i+j] = b[COL*i+j] - tmp[COL*i+j];
+            z[COL*i+j] = r[COL*i+j];
+        }
+    }
+    
+    for (it=1;it<itmax;it++)
+    {
+        vmdot(A,z,tmp);
+        alpha = vvdot(r,r)/vvdot(z,tmp);
+        
+        for (i=0;i<ROW;i++){
+            for (j=0;j<COL;j++){
+                x[COL*i+j] = x[COL*i+j] + alpha * z[COL*i+j];
+            }
+        }
+        
+        vmdot(A,z,tmp);
+        
+        for (i=0;i<ROW;i++){
+            for (j=0;j<COL;j++){
+                r_new[COL*i+j] = r[COL*i+j] - alpha*tmp[COL*i+j];
+            }
+        }
+        
+        if (norm_L2(r_new) < tol )
+            break;
+        
+        beta = vvdot(r_new,r_new)/vvdot(r,r);
+        for (i=0;i<ROW;i++){
+            for (j=0;j<COL;j++){
+                z[COL*i+j] = r_new[COL*i+j] + beta*z[COL*i+j];
+                r[COL*i+j] = r_new[COL*i+j];
+            }
+        }
+    }
+    
 }
 
 void make_Abx(double (*A)[ROW*COL],double *b,double *x,
@@ -110,6 +155,13 @@ double norm_L2(double *a)
 void vmdot(double (*A)[ROW*COL],double *x,double *b)
 {
     int i,j;
+    
+    for (i=0;i<ROW*COL;i++){
+        for (j=0;j<ROW*COL;j++){
+            b[i] = 0;
+        }
+        
+    }
     
     for (i=0;i<ROW*COL;i++){
         for (j=0;j<ROW*COL;j++){
